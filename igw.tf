c37cb4efda54008e33d51c56e6ade05c03ae5ae8
asmitha <140152177+asmitha456@users.noreply.github.com>
2025-11-18 11:20:13 +0530

    Update igw.tf

--- igw.tf
+++ igw.tf
@@ -1,7 +1,7 @@
 #Create Internet Gateway
-resource "aws_internet_gateway" "main-igw"{
-  vpc_id=aws_vpc.vpc-main.id
-  tags={
-    Name="igw-main"
+resource "aws_internet_gateway" "main-igw" {
+  vpc_id = aws_vpc.vpc-main.id
+  tags = {
+    Name = "igw-main"
   }
 }
