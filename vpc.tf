a4aa417d967365b44661b63e3fe505f902a205f8
asmitha <140152177+asmitha456@users.noreply.github.com>
2025-11-18 11:27:38 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,11 @@
 #Creating VPC
-resource "aws_vpc" "vpc-main"{
-  cidr_block="10.0.0.0/16"
-  instance_tenancy="default"
-  enable_dns_support=true
-  enable_dns_hostname=true
-  enable_classiclink=false
-  tags={
-    Name="vpc-main"
+resource "aws_vpc" "vpc-main" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "vpc-main"
   }
 }
 
