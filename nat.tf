acfb692b0a30f0e1d28bf03fae781cbe10d3a690
asmitha <140152177+asmitha456@users.noreply.github.com>
2025-11-18 11:20:42 +0530

    Update nat.tf

--- nat.tf
+++ nat.tf
@@ -1,12 +1,13 @@
 #Nat Gateway
-resource "aws_eip" "nat"{
-  vpc=true
+resource "aws_eip" "nat" {
+  domain = "vpc"
 }
 
-resource aws_nat_gateway" "nat-gw"{
-  allocation_id=aws_eip.nat.id
-  subnet_id=aws_subnet.vpc-main-web-public-Az1.id
-  depends_on=[aws_internet_gateway.main-igw.id]
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.web-public-Az1.id
+  tags = {
+    Name = "nat-gw"
+  }
 }
-
 
